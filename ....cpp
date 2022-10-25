6919862d13a8bdd1877fb1758d10f04c4aa89444
udKG5C-WpzAVuD <tqq25172431@163.com>
2022-10-25 10:59:39 +0800

    commit ...

---
+++ ....cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int n,i,j;
+	scanf("%d",&n);
+	
+	int a[n][4];
+	
+	for (i=0;i<n;i++)
+	{
+		for (j=0;j<4;j++)
+		{
+			cin>>a[i][j];
+		}
+	}
+	
+	return 0;
+}
+
